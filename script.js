801ccc5065c2d300b06ff08d96a0efe7b3453a96
adriancitotomas-03 <adriantomaslopeez@gmail.com>
2026-09-10 11:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const secciones = document.querySelectorAll(".godofwar, .johnwick, .hollowknight, .dragonball, .lunadepluton"
+);
+
+const observador = new IntersectionObserver((entradas) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("visible");
+    }
+  });
+});
+
+secciones.forEach((seccion) => {
+  observador.observe(seccion);
+});
