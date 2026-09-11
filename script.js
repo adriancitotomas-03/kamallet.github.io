897ec0590c2ab86e27581f7501db668a05351fa6
adriancitotomas-03 <adriantomaslopeez@gmail.com>
2026-09-11 10:57:58 -0300

    Enhance visibility handling with new classes
    Added 'invisible' and 'saliendo' classes for better visibility control.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const secciones = document.querySelectorAll(".godofwar, .johnwick, .hollowknight, .dragonball, .lunadepluton"
+const secciones = document.querySelectorAll(
+  ".godofwar, .johnwick, .hollowknight, .dragonball, .lunadepluton"
 );
+
+secciones.forEach((seccion) => {
+  seccion.classList.add("invisible");
+});
 
 const observador = new IntersectionObserver((entradas) => {
   entradas.forEach((entrada) => {
     if (entrada.isIntersecting) {
       entrada.target.classList.add("visible");
+      entrada.target.classList.remove("invisible", "saliendo");
+    } else {
+      entrada.target.classList.add("saliendo");
+      entrada.target.classList.remove("visible", "invisible");
     }
   });
+}, {
+  threshold: 0.4,
 });
 
 secciones.forEach((seccion) => {
